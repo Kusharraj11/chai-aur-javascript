94e0395bdfd508e5e9dba4bf3dd5f3300b0ad076
kusharraj11 <kusharraj11@gmail.com>
2026-02-27 18:36:27 +0530

    27feb

--- basic_2/06_object.js
+++ basic_2/06_object.js
@@ -1,8 +1,21 @@
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
-console.log(Object.keys(student));
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// console.log(Object.keys(student));
+const course = {
+    coursename: "js in english",
+    price : "999",
+    courseInstructor: "kushar"
+}
+// course.courseInstructor
+const {courseInstructor : instructor} = course //object de-structure
+//console.log (courseInstructor);
+console.log (instructor);
 
+{
+    name : "kushar",
+    coursename:
 
+}
 
